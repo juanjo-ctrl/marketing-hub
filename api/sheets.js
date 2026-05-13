c2ebad99c5dcde8b499c8a9c0695ffa76beedcc8
Juanjo <juanjo@mcm.cm>
2026-05-13 13:11:48 +0200

    Variables de entorno seguras

--- api/sheets.js
+++ api/sheets.js
@@ -4,15 +4,15 @@ const auth = new google.auth.GoogleAuth({
   credentials: {
     type: "service_account",
     project_id: "marketing-hub-496210",
-    private_key_id: "5d4bb791dbfa3862f841e2821083c26c5175cb1b",
-    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUZcJ0NSMv9w9a\nhw7BpS0x2trBgtIR+dXpAb/3502zDePdPW/w+ICjE2CvwN2pSvK3YJa0iYndvN6a\nPJUQGtL3gKqWcGgGheO0bWw1wyLBDpzHfkz6zUwErSLyRg1Nq8YKitI2vn6aJkgg\nG+cLSuF57IEyQk3KSYZYRgKdiGid8In5bzSbEVO5Tfkx0k4zi0zaXx/S0ThQWI5Z\nbTrhnJIc8ovrUfnYRXo/dRzvy/nauMzj5GYZ8Ke8qMcVTX8ZUJ9YY85XlMjDsEbH\nqcmtBOUG8ymBErx996Y3yhSpFYF9VqeKRjKXjeEztVn4KwB4QGurfMzDwfFGsBP8\ngRf+vhqnAgMBAAECggEANTDgwFJ4Y/A256BwGR3BEj2xUAnvivHq2KYICdDi3AcL\n/CGBMbNCH9Tf/1PJDzcGTtdBFqhlHuNISg8G85gMIlCWKAOxgYSxUEOg+FvMjdWX\nfLxMEfks4vTrDgpuFXgBqi021N/i+76/jE8AjwrrKaoJduk06wZ7vwqVaBc7v+WC\ntuQPiBRQUreEoXA0imnnkOAVFHud7IhGONYVU0wGGdQcP6mbUw4YxLcwk6ls6CyX\nXwsSr7qgHuFBcGrd+JaNFTM6uXETNy4jEeotsgGzt3+beP3T97eq+FQJJuzkhR6i\nmFUtN4n4KX6EKAPWY54dM0ZTnR2PAM1/w/xY12Fu1QKBgQD8bt/XOptzBsgR2+cB\nafglOeFff92ntJZqqxj0EXit+kOANLro2v+48UP0elceySgkZiCSpPs46hDySliE\nU3ThPgofgkJIczSl9dG5LYmJpg9fwfUjY4J8S1wxjC8kn9SXvbiXBmLR+XnPJ6Me\nOWHk7uc4XdjcOyK2F1VIuZSFuwKBgQDXZhCELJ+UhFMckGPrKJMRvx2FWvM3IwEe\nniRDjBLOJxBAHimF0pkS6JqQWckrP3Y6FGT4xyGXwleQItaevQWWyNq5q8/J5LI3\ngohjicMwfy8AXmV3Q1hp+evbGKAWYNj0PvOT9bGMiJPw2Pgmopga0xVz5p0UamMg\nbpoo0l+aBQKBgQCbHUpgx/ujiARCrRzbCpHX1iQ9+aVrmxVewHpZ565/QaxeyIgd\n9Ax1SZPXU6HbIgKPs03KrqRMyc/WAoc38Mx+aA/yYql2OYydelr/7hJ4ydjin52R\nvGD/HZOuQpTpbWlGQOd/rgXlPuZxu8qJFPupoH/mbAyI77GZsObQBHFWZwKBgHtg\nCKHMdXM5/+jig3jUeri1gRE7MQgp0qBY8GiIONU/5rzzCdGO+QKtnKqF4wYUiYFm\n4Rbl6EXmmQUED25I0oS1cyLaWGBJ0BKff8OTyutiTeemKfQP7Pp2njOWfUuUGXB9\nBkN/fT0DlclkXW6zvu/ObAFA3EQvU6vl7gLnzL6JAoGBAN0Vr18B5ujidDxaYUYk\neGJ75sZkMURScfixdHmA1TVvYcypZDBOLnie20Y1JsAf85KF+OgcXDs5YPKmcXd/\ndMax+AJ82JC/D2PXBTCs5WjnnmSDa6gKZtgf8DZmlbL9jo07yY9vT0+Z/+YFpWbt\n4x/QulTJo2rHUq6PCMiSCpA0\n-----END PRIVATE KEY-----\n",
-    client_email: "marketing-hub@marketing-hub-496210.iam.gserviceaccount.com",
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: "114920605552035358827",
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-const SPREADSHEET_ID = '1ZaYA-dER29TE40tj4I5aPZ62fVhnE14R9REt397YId8';
+const SPREADSHEET_ID = process.env.GOOGLE_SPREADSHEET_ID;
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
